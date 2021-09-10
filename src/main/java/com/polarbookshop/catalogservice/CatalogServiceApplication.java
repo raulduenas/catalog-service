31e3d36938cf0eec0cf18a1d01f988c8b3cd6f70
Raúl Dueñas <raulduenas29@gmail.com>
2021-09-10 16:09:51 -0500

    fin capitulo 3

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.catalogservice;
+package com.polarbookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
